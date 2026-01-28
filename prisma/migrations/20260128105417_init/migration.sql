29761f20d85b3a502459b187fd3a37af0a72f4d5
SergioMonteRi <sergioribeiropalermo@gmail.com>
2026-01-28 07:55:35 -0300

    feat(prisma): initialize Prisma setup with PostgreSQL datasource and appointments model

---
+++ prisma/migrations/20260128105417_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "appointments" (
+    "id" TEXT NOT NULL,
+    "tutorName" TEXT NOT NULL,
+    "petName" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "scheduledAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "appointments_pkey" PRIMARY KEY ("id")
+);
