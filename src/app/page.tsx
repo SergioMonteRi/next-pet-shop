c4a650ea12eb85b9facffa48e3bb9b6fd2c55b47
SergioMonteRi <sergioribeiropalermo@gmail.com>
2026-01-28 08:27:12 -0300

    refactor: update layout and page content for Mundo Pet application

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-accent-blue">Hello World</h1>
+    <div className="bg-background-primary p-6">
+      <div className="flex items-center justify-between mb-8">
+        <div className="flex flex-col gap-2">
+          <h1 className="text-title-size text-content-primary">Sua agenda</h1>
+          <p className="text-paragraph-small-size text-content-secondary">
+            Aqui você pode ver todos os clientes e serviços agendados para hoje.
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
